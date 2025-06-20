024829e987bbf994fedfc720b82ded2e97670619
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-06-20 18:50:08 +0500

    Controller

--- my-app/app/Http/Controllers/UserController.php
+++ my-app/app/Http/Controllers/UserController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        return 'Bu narsalarni ro`yhati';
+    }
+
+    public function show($user){
+        return 'Tanlangan user ' . $user;
+    }
+
+    public function create(){
+        return view('users.create');
+    }
+
+    public function edit($user_id){
+        return $user_id . ' ni O \'zgartirish';
+    }
 }
--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -1,21 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/test', function () {
-//     return view("test");
-// });
-// ikkalasi bir narsa ==
-Route::view('/test', 'test-route');
-
-Route::get('user/{id}/photo/{photo_id}', function ($user_id, $photo_id) {
-    return 'Siz tanlagan user id bu ' . $user_id . ' Siz tanlagan photo id bu ' . $photo_id;
-});
-
-Route::post('contact', function(){
-    return 'post route';
-});
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/create', [UserController::class, 'create']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::get('/users/{user}/edit', [UserController::class, 'edit']);
