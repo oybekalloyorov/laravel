44bb9d7f891d0797dc857b98e5becf11ab8fbbd7
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-06-23 18:53:42 +0500

    klean web template -- My first web site in laravel

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\TestConteroller;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'main']);
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/create', [UserController::class, 'create']);
-Route::get('/users/{user}', [UserController::class, 'show']);
-Route::get('/users/{user}/edit', [UserController::class, 'edit']);
 
-Route::get('/test/{testuser}', [TestConteroller::class, 'testIndex']);
-Route::post('/user-create', [UserController::class, 'store']);
+
+
+
+
+// Route::get('/users', [UserController::class, 'index']);
+// Route::get('/users/create', [UserController::class, 'create']);
+// Route::get('/users/{user}', [UserController::class, 'show']);
+// Route::get('/users/{user}/edit', [UserController::class, 'edit']);
+
+// Route::post('/user-create', [UserController::class, 'store']);
